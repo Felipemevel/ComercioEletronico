dab559cd8b9549aa410789fa8a3e7eaa89968813
Felipe Nascimento <lipe.conta1632@gmail.com>
2026-05-31 09:59:44 -0300

    VendaItemDAO criado.

---
+++ src/main/java/dao/VendaItemDAO.java
@@ -0,0 +1,70 @@
+package dao;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import model.VendaItem;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+public class VendaItemDAO {
+
+    private List<VendaItem> objetos = new ArrayList<>();
+    private ObjectMapper mapper = new ObjectMapper();
+    private final String ARQUIVO = "vendaitem.json";
+
+    public VendaItemDAO(){
+        mapper.enable(SerializationFeature.INDENT_OUTPUT);
+    }
+
+    public void abrir(){
+        try {
+            File arquivo = new File(ARQUIVO);
+            if (arquivo.exists()){
+                objetos = mapper.readValue(arquivo, new TypeReference<List<VendaItem>>(){});
+            }
+        } catch (Exception e) {objetos = new ArrayList<>();}
+    }
+    public void salvar(){
+        try {
+            File arquivo = new File(ARQUIVO);
+            mapper.writeValue(arquivo, objetos);
+        } catch (Exception e) {
+            System.out.println("Não foi possível salvar: " + e.getMessage());
+        }
+    }
+    public void inserir(VendaItem obj){
+        objetos.add(obj);
+        salvar();
+    }
+    public List<VendaItem> listar(){
+        return objetos;
+    }
+    public VendaItem listarId(int id){
+        for (VendaItem x : objetos){
+            if (x.getId() == id){
+                return x;
+            }
+        }
+        return null;
+    }
+    public void excluir(VendaItem obj){
+        VendaItem vendaItemEncontrado = listarId(obj.getId());
+        if (vendaItemEncontrado != null){
+            objetos.remove(vendaItemEncontrado);
+            salvar();
+        }
+    }
+    public void atualizar(VendaItem obj){
+        VendaItem vendaItemEncontrado = listarId(obj.getId());
+        if (vendaItemEncontrado != null){
+            vendaItemEncontrado.setIdProduto(obj.getIdProduto());
+            vendaItemEncontrado.setPreco(obj.getPreco());
+            vendaItemEncontrado.setQuantidade(obj.getQuantidade());
+            vendaItemEncontrado.setIdVenda(obj.getIdVenda());
+            salvar();
+        }
+    }
+}

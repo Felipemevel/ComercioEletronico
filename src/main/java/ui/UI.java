8f371d4cad39954335ab3c5aa6d171a356af8704
Felipe Nascimento <lipe.conta1632@gmail.com>
2026-06-03 11:19:28 -0300

    UI: Métodos de cliente, produtos e categorias adicionados.

--- src/main/java/ui/UI.java
+++ src/main/java/ui/UI.java
@@ -3,6 +3,7 @@ package ui;
 import dao.CategoriaDAO;
 import dao.ClienteDAO;
 import dao.ProdutoDAO;
+import model.Categoria;
 import model.Cliente;
 import model.Produto;
 import util.Util;
@@ -88,8 +89,8 @@ public class UI {
             switch (option){
                 case 1 -> inserirProduto();
                 case 2 -> listarProdutos();
-                case 3 -> System.out.println("Em construção...");
-                case 4 -> System.out.println("Em construção...");
+                case 3 -> atualizarProduto();
+                case 4 -> removerProduto();
                 case 5 -> System.out.println(">>> Voltando ao menu principal...");
                 default -> System.out.println(">>> Opção inválida!");
             }
@@ -112,10 +113,10 @@ public class UI {
             option = Util.convInt(sc, "> Digite a opção: ");
 
             switch (option){
-                case 1 -> System.out.println("Em construção...");
-                case 2 -> System.out.println("Em construção...");
-                case 3 -> System.out.println("Em construção...");
-                case 4 -> System.out.println("Em construção...");
+                case 1 -> inserirCategoria();
+                case 2 -> listarCategorias();
+                case 3 -> atualizarCategoria();
+                case 4 -> removerCategoria();
                 case 5 -> System.out.println(">>> Voltando ao menu principal...");
                 default -> System.out.println(">>> Opção inválida!");
             }
@@ -128,9 +129,9 @@ public class UI {
     public static void inserirCliente(){
         System.out.println("\n=== INSERIR NOVO CLIENTE ===");
 
-        String nome = Util.lerTexto(sc, "> Insira o nome do cliente");
-        String fone = Util.lerTexto(sc, "> Insira o telefone do cliente");
-        String email = Util.lerTexto(sc, "> Insira o e-mail do cliente");
+        String nome = Util.lerTexto(sc, "> Insira o nome do cliente: ");
+        String fone = Util.lerTexto(sc, "> Insira o telefone do cliente: ");
+        String email = Util.lerTexto(sc, "> Insira o e-mail do cliente: ");
 
         Cliente novoCliente = new Cliente(0, nome, email, fone);
         clienteDAO.inserir(novoCliente);
@@ -141,19 +142,19 @@ public class UI {
     public static void listarClientes(){
         var lista = clienteDAO.listar();
         if (lista.isEmpty()){
-            System.out.println("Nenhum cliente cadastrado no sistema.");
+            System.out.println(">>> Nenhum cliente cadastrado no sistema.");
             return;
         }
         System.out.println("\n=== LISTA DE CLIENTES ===");
         for (Cliente c : lista){
-            System.out.println("= " + c);
+            System.out.println(c);
             System.out.println("-----------------------------------");
         }
 
     }
 
     public static void atualizarCliente(){
-        System.out.println("\n=== INSERIR NOVO CLIENTE ===");
+        System.out.println("\n=== ATUALIZAR CLIENTE ===");
 
         int id = Util.convInt(sc, "> Insira o ID do cliente que deseja atualizar os dados: ");
         Cliente clienteDesejado = clienteDAO.listarId(id);
@@ -172,6 +173,8 @@ public class UI {
 
         Cliente clienteAtualizado = new Cliente(id, nome, fone, email);
         clienteDAO.atualizar(clienteAtualizado);
+
+        System.out.println(">>> Cliente atualizado com sucesso!");
     }
 
     public static void removeCliente(){
@@ -189,6 +192,7 @@ public class UI {
         System.out.println("-----------------------------------");
 
         clienteDAO.excluir(clienteDesejado);
+        System.out.println(">>> Cliente removido com sucesso!");
     }
 
     /**
@@ -201,8 +205,9 @@ public class UI {
         String descricao = Util.lerTexto(sc, ">>> Insira a descrição do produto: ");
         double preco = Util.convDouble(sc, ">>> Insira o preço do produto: ");
         int estoque = Util.convInt(sc, ">>> Insira a quantidade de produtos no estoque: ");
+        int idCategoria = Util.convInt(sc, ">>> Insira o ID da categoria deste produto: ");
 
-        Produto novoProduto = new Produto(0, descricao, preco, estoque);
+        Produto novoProduto = new Produto(0, descricao, preco, estoque, idCategoria);
         produtoDAO.inserir(novoProduto);
 
         System.out.println(">>> Produto cadastrado com sucesso!");
@@ -211,22 +216,24 @@ public class UI {
     public static void listarProdutos(){
         var lista = produtoDAO.listar();
         if (lista.isEmpty()){
-            System.out.println("Nenhum produto cadastrado no sistema.");
+            System.out.println(">>> Nenhum produto cadastrado no sistema.");
             return;
         }
         System.out.println("\n=== LISTA DE PRODUTOS ===");
         for (Produto p : lista){
-            System.out.println("= " + p);
+            System.out.println(p);
             System.out.println("-----------------------------------");
         }
     }
 
     public static void atualizarProduto(){
+        System.out.println("\n=== ATUALIZAR PRODUTO ===");
+
         int id = Util.convInt(sc, ">>> Digite o ID do produto que deseja atualizar: ");
         Produto produtoDesejado = produtoDAO.listarId(id);
 
         if (produtoDesejado == null){
-            System.out.println(">>> Erro: Cliente com ID " + id + " não encontrado.");
+            System.out.println(">>> Erro: Produto com ID " + id + " não encontrado.");
             return;
         }
         System.out.println("\n>>> Dados atuais do cliente:");
@@ -236,8 +243,85 @@ public class UI {
         String descricao = Util.lerTexto(sc, ">>> Insira a nova descrição: ");
         double preco = Util.convDouble(sc, ">>> Insira o novo preço: ");
         int estoque = Util.convInt(sc, ">>> Insira a nova quantidade no estoque: ");
+        int idCategoria = Util.convInt(sc, ">>> Insira o novo ID de categoria do produto: ");
 
+        Produto produtoAtualizado = new Produto(id, descricao, preco, estoque, idCategoria);
+        produtoDAO.atualizar(produtoAtualizado);
 
-        Produto produtoAtualizado = new Produto(id, )
+        System.out.println(">>> Categoria atualizada com sucesso!");
     }
+
+    public static void removerProduto(){
+        System.out.println("\n=== REMOVER PRODUTO ===");
+
+        int id = Util.convInt(sc, "Digite o ID do produto que deseja remover: ");
+        Produto produtoDesejado = produtoDAO.listarId(id);
+
+        if (produtoDesejado == null){
+            System.out.println(">>> Erro: Produto com ID " + id + " não encontrado.");
+            return;
+        }
+        produtoDAO.excluir(produtoDesejado);
+
+        System.out.println(">>> Produto cadastrado com sucesso!");
+    }
+
+    /**
+     *  MÉTODOS CATEGORIAS
+     */
+
+    public static void inserirCategoria(){
+        System.out.println("\n=== INSERIR NOVA CATEGORIA ===");
+
+        String descricao = Util.lerTexto(sc, ">>> Insira a descrição da nova categoria: ");
+        Categoria novaCategoria = new Categoria(0, descricao);
+        categoriaDAO.inserir(novaCategoria);
+
+        System.out.println(">>> Categoria cadastrada com sucesso!");
+    }
+
+    public static void listarCategorias(){
+        var lista = categoriaDAO.listar();
+        if (lista.isEmpty()){
+            System.out.println(">>> Nenhuma categoria cadastrada no sistema.");
+            return;
+        }
+        for (Categoria c : lista){
+            System.out.println(c);
+            System.out.println("-----------------------------------");
+        }
+    }
+
+    public static void atualizarCategoria(){
+        System.out.println("\n=== ATUALIZAR CATEGORIA ===");
+
+        int id = Util.convInt(sc, ">>> Insira o ID da categoria que deseja atualizar: ");
+        Categoria categoriaDesejada = categoriaDAO.listarId(id);
+
+        if (categoriaDesejada == null){
+            System.out.println(">>> Erro: Categoria com ID " + id + " não encontrado.");
+            return;
+        }
+        String descricao = Util.lerTexto(sc, ">>> Digite a nova descrição: ");
+        Categoria categoriaAtualizada = new Categoria(id, descricao);
+        categoriaDAO.atualizar(categoriaAtualizada);
+
+        System.out.println(">>> Categoria atualizada com sucesso!");
+    }
+
+    public static void removerCategoria(){
+        System.out.println("\n=== REMOVER CATEGORIA ===");
+
+        int id = Util.convInt(sc, ">>> Digite o ID da categoria que você deseja remover: ");
+        Categoria categoriaDesejada = categoriaDAO.listarId(id);
+
+        if (categoriaDesejada == null){
+            System.out.println(">>> Erro: Categoria com ID " + id + " não encontrado.");
+            return;
+        }
+        categoriaDAO.excluir(categoriaDesejada);
+
+        System.out.println(">>> Categoria removida com sucesso!");
+    }
+
 }
